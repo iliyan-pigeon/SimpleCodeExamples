58aa3ef3ad4367477fa7863069155c836b5e1bcf
Iliyan Galabov <milenov556@gmail.com>
2024-06-14 16:51:27 +0300

    Update face_recognition.py

--- face_recognition.py
+++ face_recognition.py
@@ -20,18 +20,25 @@ def find_face_location(image_path):
         return None
 
 
-def enhance_and_greyscale_image(image_path, brightness=1.5, contrast=1.5, sharpness=1.5):
+def enhance_and_greyscale_image(image_path, contrast=2.0, sharpness=2.0):
     face_locations = find_face_location(image_path)
     top, right, bottom, left = face_locations[0]
 
     pil_image = Image.open(image_path)
     image = pil_image.crop((left, top, right, bottom))
 
+    width, height = image.size
+    max_width = 300
+    ratio = max_width / width
+    new_width = max_width
+    new_height = int(height * ratio)
+    image = image.resize((new_width, new_height))
+
     os.remove(image_path)
 
-    # Enhance the brightness
-    enhancer = ImageEnhance.Brightness(image)
-    image = enhancer.enhance(brightness)
+    image = image.filter(ImageFilter.DETAIL)
+    image = image.filter(ImageFilter.ModeFilter(size=2))
+    image = image.filter(ImageFilter.SMOOTH)
 
     # Enhance the contrast
     enhancer = ImageEnhance.Contrast(image)
@@ -46,7 +53,6 @@ def enhance_and_greyscale_image(image_path, brightness=1.5, contrast=1.5, sharpn
 
     # Save the grayscale image
     gray_image.save(image_path)
-    send_email(image_path)
 
     return image_path
 
@@ -58,14 +64,13 @@ def compare_faces(first_image_path, second_image_path):
     first_image = enhance_and_greyscale_image(first_image_path)
     second_image = enhance_and_greyscale_image(second_image_path)
 
+    send_email(first_image, second_image)
+
     image_one = face_recognition.load_image_file(f"{first_image}")
     image_two = face_recognition.load_image_file(f"{second_image}")
 
-    first_face_location = face_recognition.face_locations(image_one, model=model)
-    second_face_location = face_recognition.face_locations(image_two, model=model)
-
-    first_image_encoding = face_recognition.face_encodings(image_one, first_face_location)[0]
-    second_image_encoding = face_recognition.face_encodings(image_two, second_face_location)[0]
+    first_image_encoding = face_recognition.face_encodings(image_one)[0]
+    second_image_encoding = face_recognition.face_encodings(image_two)[0]
 
     result = face_recognition.compare_faces([first_image_encoding], second_image_encoding, tolerance)[0]
     face_distance = face_recognition.face_distance([first_image_encoding], second_image_encoding)[0]
